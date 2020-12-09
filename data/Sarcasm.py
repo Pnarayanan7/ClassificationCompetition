8ba08b133d975131425750d4ceab3c9492651dd3
Nadia Mohiuddin <nadiam2@illinois.edu>
2020-12-08 22:05:02 -0600

    trying svm with new transform

--- data/Sarcasm.py
+++ data/Sarcasm.py
@@ -96,18 +96,18 @@ tv = TfidfVectorizer(max_features = 5000)
 train_tfidf = tv.fit_transform(train_responses).toarray()
 test_tfidf = tv.transform(test_responses).toarray()
 
-# lsvc = LinearSVC()
-# lsvc.fit(train_tfidf, train_labels)
-# test_labels = lsvc.predict(test_tfidf)
+lsvc = LinearSVC()
+lsvc.fit(train_tfidf, train_labels)
+test_labels = lsvc.predict(test_tfidf)
 #print(test_labels)
 
-train_tfidf = np.array(train_tfidf)
-test_tfidf = np.array(test_tfidf)
-train_tfidf = train_tfidf.astype(np.float64)
-test_tfidf = test_tfidf.astype(np.float64)
-clf = GaussianNB()
-clf.fit(train_tfidf, train_labels)
-test_labels = clf.predict(test_tfidf)
+# train_tfidf = np.array(train_tfidf)
+# test_tfidf = np.array(test_tfidf)
+# train_tfidf = train_tfidf.astype(np.float64)
+# test_tfidf = test_tfidf.astype(np.float64)
+# clf = GaussianNB()
+# clf.fit(train_tfidf, train_labels)
+# test_labels = clf.predict(test_tfidf)
 
 
 #output test labels to test file
