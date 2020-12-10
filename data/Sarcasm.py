040862ecabd99102b118626b411d32f88f9e3072
Thaniel Tong <thaniel.tong@kinetixtt.com>
2020-12-10 13:05:20 -0600

    hm

--- data/Sarcasm.py
+++ data/Sarcasm.py
@@ -84,8 +84,9 @@ for i, response in enumerate(train_responses):
         word = word.replace('#', '')
         if word in stop_words:
             continue
+        word = word.lower()
         word = lemmatizer.lemmatize(word)
-        word = ps.stem(word.lower())
+#        word = ps.stem(word)
         if word not in stop_words:
             resp.append(word)
     train_responses[i] = ' '.join(resp)
@@ -100,8 +101,9 @@ for i, context_list in enumerate(train_contexts):
             word = word.replace('#', '')
             if word in stop_words:
                 continue
+            word = word.lower()
             word = lemmatizer.lemmatize(word)
-            word = ps.stem(word.lower())
+            #        word = ps.stem(word)
             if word not in stop_words:
                 context_l.append(word)
         context_list[j] = ' '.join(context_l)
@@ -121,8 +123,9 @@ for i, response in enumerate(test_responses):
         word = word.replace('#', '')
         if word in stop_words:
             continue
+        word = word.lower()
         word = lemmatizer.lemmatize(word)
-        word = ps.stem(word.lower())
+        #        word = ps.stem(word)
         if word not in stop_words:
             resp.append(word)
     test_responses[i] = ' '.join(resp)
@@ -137,8 +140,9 @@ for i, context_list in enumerate(test_contexts):
             word = word.replace('#', '')
             if word in stop_words:
                 continue
+            word = word.lower()
             word = lemmatizer.lemmatize(word)
-            word = ps.stem(word.lower())
+            #        word = ps.stem(word)
             if word not in stop_words:
                 context_l.append(word)
         context_list[j] = ' '.join(context_l)
